84bf2863bdbf6a027bb1ab2265204e234c478dd3
Zero Developer <zeroaixz14@gmail.com>
2025-09-02 15:45:21 +0700

    Update engine-requirements.js

--- engine-requirements.js
+++ engine-requirements.js
@@ -1,8 +1,8 @@
 const major = parseInt(process.versions.node.split('.')[0], 10)
-if (major < 20) {
+if (major < 18) {
 console.error(
 `\n❌ Oops! Versi Node.js kamu terlalu lawas...\n` +
-`   Diperlukan: Node.js v20 atau lebih tinggi\n` +
+`   Diperlukan: Node.js v18 atau lebih tinggi\n` +
 `   Saat ini: v${process.versions.node}\n\n` +
 `   🚀 Ayo upgrade sekarang biar gak ketinggalan zaman!\n`
 )
